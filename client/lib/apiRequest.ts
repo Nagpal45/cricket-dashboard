a2739c6bb462c37c2923235364085d186fb12d88
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-12 18:08:50 +0530

    Revert "deployment check"
    This reverts commit adb95e1f7a925e91237fa45306cf37d62c66ffab.

--- client/lib/apiRequest.ts
+++ client/lib/apiRequest.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://cricket-dashboard.onrender.com/api/",
+    baseURL: "http://localhost:5000/api/",
     withCredentials: true,
 });
 
